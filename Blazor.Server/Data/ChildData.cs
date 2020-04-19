28ef5f959be7bb87026fca8ffef7ed733a8719aa
Ervis Trupja <ervistrupja@outlook.com>
2020-04-19 21:35:49 +0200

    Blazor final

---
+++ Blazor.Server/Data/ChildData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Server.Data
+{
+    public class ChildData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
